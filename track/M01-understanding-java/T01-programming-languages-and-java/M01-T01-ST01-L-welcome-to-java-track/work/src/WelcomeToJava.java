c3ced10558fbdf25b60867d3ce2c6f28e200b174
ArjunBalagopalan <arjunbalagopalan4510@gmail.com>
2026-07-30 13:07:21 +0530

    Added new  Lines

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,8 +3,10 @@ public class WelcomeToJava {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Java Track");
+        System.out.println("My Name is Arjun Balagopalan");
         System.out.println("I already know how to build programming Logic");
         System.out.println("now i am learning to express the logic in Java");
         System.out.println("Now I know how to use git and github to keep track of my code");
+        System.out.println("My Goal is to become a Java Developer");
     }
 }
