bd9e1da14c5bdd3f69da67d138fda8f84cf1eb3d
ArjunBalagopalan <arjunbalagopalan4510@gmail.com>
2026-07-30 12:59:31 +0530

    Added One More Line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,7 +1,10 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       System.out.println("Welcome to the Java Track");
-       System.out.println("I already know how to build programming Logic");
-       System.out.println("now i am learning to express the logic in Java");
+        System.out.println("Welcome to the Java Track");
+        System.out.println("I already know how to build programming Logic");
+        System.out.println("now i am learning to express the logic in Java");
+        System.out.println("Now I know how to use git and github to keep track of my code");
     }
 }
