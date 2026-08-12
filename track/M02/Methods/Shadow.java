8c9b15009b7de568132f21e2d9c50ba1c1e681f4
ArjunBalagopalan <arjunbalagopalan4510@gmail.com>
2026-08-12 12:05:11 +0530

    Shadowing

---
+++ track/M02/Methods/Shadow.java
@@ -0,0 +1,13 @@
+
+public class Shadow {
+
+    public static void main(String[] args) {
+
+        Student1 s1 = new Student1();
+        s1.name = "RAJA";
+        s1.age = 20;
+        s1.height = 5.6;
+        s1.display();
+
+    }
+}
