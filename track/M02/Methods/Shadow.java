9a250dd70aef0fc3a29622024e3f748c1d999095
ArjunBalagopalan <arjunbalagopalan4510@gmail.com>
2026-08-12 12:14:55 +0530

    corrected the mistake

--- track/M02/Methods/Shadow.java
+++ track/M02/Methods/Shadow.java
@@ -4,9 +4,7 @@ public class Shadow {
     public static void main(String[] args) {
 
         Student1 s1 = new Student1();
-        s1.name = "RAJA";
-        s1.age = 20;
-        s1.height = 5.6;
+        s1.input("Raja", 18, 5.6);
         s1.display();
 
     }
--- track/M02/Methods/Student1.java
+++ track/M02/Methods/Student1.java
@@ -6,12 +6,9 @@ class Student1 {
     double height;
 
     void input(String name, int age, double height) {
-        // this.
-        name = name;
-        //this.
-        age = age;
-        //this.
-        height = height;
+        this.name = name;
+        this.age = age;
+        this.height = height;
     }
 
     void display() {
