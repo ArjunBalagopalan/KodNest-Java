c171f1c45cd532f14e2a5fdb53f2fd18e80fcd1c
ArjunBalagopalan <arjunbalagopalan4510@gmail.com>
2026-08-14 11:47:27 +0530

    First Constructor Call

---
+++ track/M02/Constructor/FirstCon.java
@@ -0,0 +1,16 @@
+
+class Robot {
+
+    Robot() {
+        // Print the message
+        System.out.println("Beep beep! Robot reporting for Java duty!");
+    }
+}
+
+public class FirstCon {
+
+    public static void main(String[] args) {
+        // Create one Robot object
+        Robot r = new Robot();
+    }
+}
