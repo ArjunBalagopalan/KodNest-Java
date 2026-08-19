964a211b14604cf677884f4648e1632799e254e4
ArjunBalagopalan <arjunbalagopalan4510@gmail.com>
2026-08-19 13:12:57 +0530

    3D Array

---
+++ track/M02/Array/ThreeDArray.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class ThreeDArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[][][] = new int[3][3][5];
+        for (int i = 0; i <= arr.length - 1; i++) {
+            for (int j = 0; j <= arr[i].length - 1; j++) {
+                for (int k = 0; k <= arr[i][j].length - 1; k++) {
+                    arr[i][j][k] = sc.nextInt();
+                }
+            }
+        }
+        for (int i = 0; i <= arr.length - 1; i++) {
+            for (int j = 0; j <= arr[i].length - 1; j++) {
+                for (int k = 0; k <= arr[i][j].length - 1; k++) {
+                    System.out.println(arr[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
